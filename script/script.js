173e370a7716e5cb1628d3d70bc84f148c5e9c91
Anh <minhanhle@hotmail.com>
2022-11-22 11:07:42 -0800

    add greenbackground on hover para

--- script/script.js
+++ script/script.js
@@ -1,7 +1,8 @@
 const clickMeButton = document.querySelector(".clickMe");
 
-function handleClickMeButton ()
+function handleClickMeButton(event)
 {
+    console.log(event.target)
     alert("Clicked!");
 }
 
@@ -25,8 +26,35 @@ clickMeButton.addEventListener("click", changeBGColor);
 
 function changeText ()
 {
-    if (clickMeButton.textContent )
+    if (clickMeButton.textContent == "Hello I am clicked");
     clickMeButton.textContent = "Clicked";
+     (clickMeButton.textContent == "Bye I am clicked");
+    clickMeButton.textContent = "Bye Clicked";
 }
 
 clickMeButton.addEventListener("click", changeText);
+
+function createAddButton()
+{
+    const purpleButton = document.createElement("button");
+    purpleButton.textContent = "Purple";
+    purpleButton.addEventListener("mousemove", changeBGGreen);
+    document.body.appendChild(purpleButton);
+}
+
+clickMeButton.addEventListener("click", createAddButton);
+
+function createAddPara ()
+{
+    const para = document.createElement("p");
+    para.textContent = "My para";
+    para.addEventListener("mousemove", changeBGGreen);
+    document.body.appendChild(para);
+}
+
+clickMeButton.addEventListener("click", createAddPara);
+
+function changeBGGreen (event)
+{
+    event.target.classList.add("green");
+}
